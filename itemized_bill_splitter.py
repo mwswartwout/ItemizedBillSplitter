feec7ec94c104fe666f249259b9c4761d43acbe2
EC2 Default User <ec2-user@ip-172-31-29-50.us-east-2.compute.internal>
2022-03-23 05:25:26 +0000

    Add BillByShares calculator

--- itemized_bill_splitter.py
+++ itemized_bill_splitter.py
@@ -12,7 +12,7 @@ class Item:
     def __repr__(self) -> str:
         return 'Item(' + self.name + ',' + str(self.amount) + ')'
 
-class Bill:
+class ItemizedBill:
     def __init__(self, items: List[Item], tax: float, tip: float) -> None:
         self.items = items
         self.tax = tax
@@ -84,9 +84,46 @@ class Bill:
        
          
         return [Item(name, round(amount, 2)) for name, amount in final_amounts.items()]
+
+class SharesItem:
+    def __init__(self, name: str, shares: int) -> None:
+        assert name
+        assert shares > 0
+
+        self.name = name
+        self.shares = shares
         
+
+class BillByShares():
+    def __init__(self, items: List[SharesItem], total: float) -> None:
+        assert items
+        assert total > 0
+
+        self.items = items
+        self.total = total
+        
+    def getSplit(self) -> List[Item]:  
+        total_shares = 0
+        for item in self.items:
+            total_shares += item.shares
+        print(total_shares)
+            
+        percentages = {}
+        for item in self.items:
+            assert item.name not in percentages
+            percentages[item.name] = item.shares / total_shares
+        print(percentages)
+        
+        final_amounts = {}
+        for name, percent in percentages.items():
+            assert name not in final_amounts
+            final_amounts[name] = percent * self.total
+        
+        return [Item(name, round(amount, 2)) for name, amount in final_amounts.items()]
+            
 def main():
-    bill = Bill([Item('all', 18), Item('Kyle', 25), Item('Rachel', 25), Item('Scott', 25), Item('Tyler', 23), Item('Sarah', 18), Item('Matt', 68)],16.16,43.63)
+    # bill = ItemizedBill([Item('all', 18), Item('Kyle', 25), Item('Rachel', 25), Item('Scott', 25), Item('Tyler', 23), Item('Sarah', 18), Item('Matt', 68)],16.16,43.63)
+    bill = BillByShares([SharesItem('Kyle', 3), SharesItem('Rachel', 3), SharesItem('Tyler', 3), SharesItem('Matt', 3), SharesItem('Scott', 2)], 1428.7)
     print(bill.getSplit()) 
 
 if __name__ == "__main__":
